eac1b031f6f77fd3031d8174ed2c2461479ac103
anjali <sharma.anjali1026@gmail.com>
2019-01-06 00:13:27 +0530

    remove file

--- SSPAdminCenter/src/app/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './pipes/shared-pipes.module';
-export * from './guard';
--- SSPAdminCenter/src/app/shared/interfaces/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    userName: string;
-    password: string;
-    deviceId: string;
-    deviceType: string;
-}
